7986684aed7b8fad7cd28657f81e375afea53138
karansharma01 <karan.sharma01@nagarro.com>
2023-07-06 19:24:17 +0530

    refines dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './DropDown.css';
 import Dropdown from './Components/Dropdown';
-
+import data from "./Services/Constants/DropDownValues.json";
+import value from "./Services/Constants/DropDownSelectedValue";
 function App() {
   return (
     <div className="App">
       <div className='inner-div'>
-        <Dropdown></Dropdown>
+        <Dropdown width='15rem' allValues={data} selectedValue={value}></Dropdown>
 
       </div>
      
