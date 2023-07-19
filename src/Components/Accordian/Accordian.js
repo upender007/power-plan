8d460a7c8208b78881aee02dd7059e6b7a7498fd
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2023-07-19 18:45:35 +0530

    accordian click feature update

--- src/Components/Accordian/Accordian.js
+++ src/Components/Accordian/Accordian.js
@@ -18,11 +18,11 @@ export default function Accordian(){
               {
                 AccordianData.map((value,index)=>(
                     <div className="accordian-child" key={index}>
-                         <div className="accordian-question">
-                         <p onClick={()=>{handler(index)}}>{value.question}</p>
-                         <p>{indexes.includes(index) ? "❌" : "➕"}</p>
+                         <div className="accordian-question" onClick={()=>handler(index)}>
+                         <p>{value.question}</p>
+                         <p className="open-close">{indexes.includes(index) ? "❌" : "➕"}</p>
                          </div>
-                         {indexes.includes(index) && <p>{value.answer}</p>}
+                         {indexes.includes(index) && <p className="accordian-answer">{value.answer}</p>}
                     </div>
                 ))
               }
